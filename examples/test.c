8b69c71b60453150199173bd1aa1833cc8d5bc59
mitchellgarrett <45768600+mitchellgarrett@users.noreply.github.com>
2021-05-11 12:33:43 -0500

    Update test.c

--- examples/test.c
+++ examples/test.c
@@ -5,11 +5,13 @@ int main(int argc, char* argv[]) {
 	initScreen();
 	setCursor(0);
 
-	//drawLine(0, 0, screenWidth() - 1, screenHeight() - 1, '+');
-	//drawLineHorizontal(0, 0, screenWidth() - 1, '#');
-	//drawLineVertical(0, 0, screenHeight() - 1, '#');
-	//drawBox(0, 0, 5, 5, '@');
-	//drawBoxFill(1, 1, 3, 3, '+');
+	// Width = 3/2 * Height
+	// Aspect ratio = 3:2
+	printsat(10, 10, "######");
+	printsat(10, 11, "######");
+	printsat(10, 12, "######");
+	printsat(10, 13, "######");
+
 	while (getKey() != 'q');
 
 	closeScreen();
